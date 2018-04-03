cedea7a49cb8d0084cb81a1655f51286d39ea4a1
fanwenzhe <523935688@qq.com>
2018-04-03 09:47:27 +0800

    Initial Commit

---
+++ 重力感应器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  重力感应器
+//
+//  Created by apple on 2018/4/3.
+//  Copyright © 2018年 范文哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 重力感应器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  重力感应器
+//
+//  Created by apple on 2018/4/3.
+//  Copyright © 2018年 范文哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
